8245112714bcd5796071178128bb99d47ed88386
Lamin Sawo <laminsawojadama@outlook.com>
2020-01-05 19:50:12 +0000

    Made the program dynamic so that users input values for X and Y User inputs are formatted as integers
    Signed-off-by: laminsawo <laminsawojadama@outlook.com>

--- python-fundamentals/day-1/basic-operators.py
+++ python-fundamentals/day-1/basic-operators.py
@@ -7,17 +7,25 @@ Operators and usage:
 / = division of numbers
 """
 # Examples:
-x = 50
-y = 10
+#x = 50
+#y = 10
+
+# Improvements: Make the variables dynamic in the sense that the user gets asked to input values for x and y
+# Comment out the example static variables
+x = int(input("Enter value for X: "))
+y = int(input("Enter value for Y: "))
 
 # adding the numbers
 a = x + y
-print("X plus Y is %d" %a)
+print(" %d plus %d is %d" %(x, y, a))
 
 # subtracting the numbers
 a = x - y
-print("X minus Y is %d" %a)
+print(" %d minus %d is %d" %(x, y, a))
 
 # multiplying the numbers
 a = x * y
-print("X multiplied by Y is %d" %)
+print(" %d multiplied by %d is %d" %(x, y, a))
+
+a = x / y
+print(" %d divided by %d is %d" %(x, y, a))
